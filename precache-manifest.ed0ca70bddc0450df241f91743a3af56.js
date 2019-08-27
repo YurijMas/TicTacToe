52d9b1c3552fc8bc6a00619f07e0120346a07c3f
Yuri Maslennikov <yurij-maslennikov@yandex.ru>
2019-08-27 11:26:43 +0300

    Updates

---
+++ precache-manifest.ed0ca70bddc0450df241f91743a3af56.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04aa5d24a21b15f9f4d5a0c4eb572caf",
+    "url": "/tictactoe/index.html"
+  },
+  {
+    "revision": "15cf7e7a36e4cc9b5903",
+    "url": "/tictactoe/static/css/main.f4b9b883.chunk.css"
+  },
+  {
+    "revision": "685d0c597e8419129599",
+    "url": "/tictactoe/static/js/2.0e3f6a1a.chunk.js"
+  },
+  {
+    "revision": "15cf7e7a36e4cc9b5903",
+    "url": "/tictactoe/static/js/main.bc54081b.chunk.js"
+  },
+  {
+    "revision": "e9d42febdd2440e2e0cd",
+    "url": "/tictactoe/static/js/runtime~main.1cfd47c4.js"
+  }
+]);
